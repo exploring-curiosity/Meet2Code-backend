8ab6ff8ddc094987cea1538a822e1f8ba52a5dcc
exploring-curiosity <curiositywake@gmail.com>
2022-06-01 22:26:16 +0530

    Added New git Config

--- config.js
+++ config.js
@@ -1,6 +1,10 @@
+// let gitConfig={
+//     clientId:"3cff0488ebac76a131ef",
+//     clientSecret:"c0cd850d3c5901a9cea50dcda54c4f7668da4bf3"
+// }
 let gitConfig={
-    clientId:"3cff0488ebac76a131ef",
-    clientSecret:"c0cd850d3c5901a9cea50dcda54c4f7668da4bf3"
+    clientId:"97a41e615a718465e715",
+    clientSecret:"df44e929b63a6545f0ef9e2771299b368718d156"
 }
 exports.serverEndPoint="https://meet2code.herokuapp.com"
 exports.clientEndPoint="https://meet2code.netlify.app"
