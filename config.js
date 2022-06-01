0ff70acc71d616bb260d8b4039335e454be31782
exploring-curiosity <curiositywake@gmail.com>
2022-06-01 18:56:47 +0530

    Added new domain

--- config.js
+++ config.js
@@ -3,5 +3,5 @@ let gitConfig={
     clientSecret:"c0cd850d3c5901a9cea50dcda54c4f7668da4bf3"
 }
 exports.serverEndPoint="https://meet2code.herokuapp.com"
-exports.clientEndPoint="https://meet2code.netlify.app/"
+exports.clientEndPoint="https://meet2code.netlify.app"
 exports.gitConfig=gitConfig
